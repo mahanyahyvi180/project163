d87a146c84adaa6a2c54a3e42a3c88b6fe0d12b1
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-09-02 21:02:56 +0330

    Add files via upload

---
+++ WindowsFormsApp53/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp53
+{
+    public class student
+    {
+        private string cd;
+        public string DateofBorn
+        {
+            get { return cd; }
+            set
+            {
+
+                System.Globalization.PersianCalendar P = new System.Globalization.PersianCalendar();
+
+                cd = P.GetYear(DateTime.Parse(value)).ToString() + "/" + P.GetMonth(DateTime.Parse(value)).ToString() + P.GetDayOfMonth(DateTime.Parse(value)).ToString();
+            }
+
+
+        }
+
+    }
+}
---
+++ WindowsFormsApp53/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp53
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            student s = new student();
+            s.DateofBorn = "2012/05/02";
+            label1.Text = s.DateofBorn;
+        }
+    }
+}
